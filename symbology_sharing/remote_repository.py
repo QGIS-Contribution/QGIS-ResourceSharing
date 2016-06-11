90f1b7b5f3e0ba1989542701382dcd94c2975ff6
Akbar Gumbira <akbargumbira@gmail.com>
2016-06-12 00:34:29 +0200

    Rename third_party module.

--- symbology_sharing/remote_repository.py
+++ symbology_sharing/remote_repository.py
@@ -4,7 +4,7 @@ from PyQt4.QtNetwork import QNetworkRequest
 
 from qgis.core import QgsNetworkAccessManager
 
-from lib.giturlparse import parse, validate
+from ext_libs.giturlparse import parse, validate
 
 
 class RemoteRepository(QObject):

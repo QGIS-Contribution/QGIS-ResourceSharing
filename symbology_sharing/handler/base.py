ef173ec047d95f4ec78d7ec2e2d8bebbb4157218
Akbar Gumbira <akbargumbira@gmail.com>
2016-06-15 00:07:00 +0200

    Remove unused import.

--- symbology_sharing/handler/base.py
+++ symbology_sharing/handler/base.py
@@ -1,7 +1,5 @@
 # coding=utf-8
-import abc
-
-from PyQt4.QtCore import QObject, QSettings, QCoreApplication, QUrl
+from PyQt4.QtCore import QObject, QCoreApplication, QUrl
 from PyQt4.QtNetwork import QNetworkRequest
 
 from qgis.core import QgsNetworkAccessManager

180e887ceeed3b8dc1c339d24b3c89228fe174c6
Akbar Gumbira <akbargumbira@gmail.com>
2016-07-11 18:39:58 +0200

    Rewrite symbol_xml_extractor module to load directly QgsSymbolV2 and QgsVectorColorRampV2

--- symbology_sharing/symbol_xml_extractor.py
+++ symbology_sharing/symbol_xml_extractor.py
@@ -1,11 +1,14 @@
 # coding=utf-8
-from xml.dom.minidom import parse
+from PyQt4.QtXml import QDomDocument
+from PyQt4.QtCore import QFile, QIODevice
+from qgis.core import QgsSymbolLayerV2Utils
 
 
 class SymbolXMLExtractor():
-    """A class which parses the given file and returns the symbols"""
-    def __init__(self, xml_file):
-        self._xml_dom = parse(xml_file)
+    """A class which parses the given file and returns the symbols and
+    colorramps"""
+    def __init__(self, xml_path):
+        self._xml_path = xml_path
         self._symbols = []
         self._colorramps = []
 
@@ -13,29 +16,76 @@ class SymbolXMLExtractor():
         self.parse_xml()
 
     def parse_xml(self):
-        symbols_dom = self._xml_dom.getElementsByTagName("symbol")
-        colorramps_dom = self._xml_dom.getElementsByTagName("colorramp")
+        """Parse the xml file. Returns false if there is failure."""
+        xml_file = QFile(self._xml_path)
+        if not xml_file.open(QIODevice.ReadOnly):
+            return False
 
+        document = QDomDocument()
+        if not document.setContent(xml_file):
+            return False
+
+        xml_file.close()
+
+        document_element = document.documentElement()
+        if document_element.tagName() != 'qgis_style':
+            return False
+
+        # Get all the symbols
         self._symbols = []
-        for symbol_dom in symbols_dom:
-            symbol = {
-                'name': symbol_dom.getAttribute('name'),
-                'xml': symbol_dom.toxml()
-            }
-            self._symbols.append(symbol)
+        symbols_element = document_element.firstChildElement('symbols')
+        symbol_element = symbols_element.firstChildElement()
+        while not symbol_element.isNull():
+            if symbol_element.tagName() == 'symbol':
+                symbol = QgsSymbolLayerV2Utils.loadSymbol(symbol_element)
+                if symbol:
+                    self._symbols.append({
+                        'name': symbol_element.attribute('name'),
+                        'symbol': symbol
+                    })
+            symbol_element = symbol_element.nextSiblingElement()
 
+        # Get all the colorramps
         self._colorramps = []
-        for colorramp_dom in colorramps_dom:
-            colorramp = {
-                'name': colorramp_dom.getAttribute('name'),
-                'xml': colorramp_dom.toxml()
-            }
-            self._colorramps.append(colorramp)
+        ramps_element = document_element.firstChildElement('colorramps')
+        ramp_element = ramps_element.firstChildElement()
+        while not ramp_element.isNull():
+            if ramp_element.tagName() == 'colorramp':
+                colorramp = QgsSymbolLayerV2Utils.loadColorRamp(ramp_element)
+                if colorramp:
+                    self._colorramps.append({
+                        'name': ramp_element.attribute('name'),
+                        'colorramp': colorramp
+                    })
+
+            ramp_element = ramp_element.nextSiblingElement()
+
+        return True
 
     @property
     def symbols(self):
+        """Return list of symbols in the xml.
+
+        The structure of the property:
+        symbols = [
+            {
+                'name': str
+                'symbol': QgsSymbolV2
+            }
+        ]
+        """
         return self._symbols
 
     @property
     def colorramps(self):
+        """Return list of colorramps in the xml.
+
+        The structure of the property:
+        colorramps = [
+            {
+                'name': str
+                'colorramp': QgsVectorColorRampV3
+            }
+        ]
+        """
         return self._colorramps

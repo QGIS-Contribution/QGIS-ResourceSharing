8eb8a068f230fee76e3624628081dd5532e0d4f9
Akbar Gumbira <akbargumbira@gmail.com>
2016-08-05 15:32:41 +0200

    Remove ext_libs path in run-env-linux.sh

--- scripts/run-env-linux.sh
+++ scripts/run-env-linux.sh
@@ -8,7 +8,7 @@ fi
 export QGIS_PREFIX_PATH=${QGIS_PREFIX_PATH}
 export QGIS_PATH=${QGIS_PREFIX_PATH}
 export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib
-export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:${EXT_LIBS_PATH}:${PYTHONPATH}
+export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:${PYTHONPATH}
 
 echo "QGIS PATH: $QGIS_PREFIX_PATH"
 export QGIS_DEBUG=0

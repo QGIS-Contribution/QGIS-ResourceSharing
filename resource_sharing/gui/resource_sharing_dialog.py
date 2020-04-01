f0d63acd4d28973d155065cf40f173424d4a31ce
Håvard Tveite <havard.tveite@nmbu.no>
2020-04-01 14:04:28 +0200

    testing

--- resource_sharing/gui/resource_sharing_dialog.py
+++ resource_sharing/gui/resource_sharing_dialog.py
@@ -523,7 +523,7 @@ class ResourceSharingDialog(QDialog, FORM_CLASS):
                          config.COLLECTIONS[coll_id]['name'])
         else:
             self.reload_collections_model()
-            currentPage = self.stacked_menu_widget.currentIndex(1)
+            currentPage = self.stacked_menu_widget.currentIndex()
             self.set_current_tab(currentPage)
             QMessageBox.information(
                 self,

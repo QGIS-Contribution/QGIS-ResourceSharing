8c9218f68547d706733ad6778c5c64efb36e9201
Håvard Tveite <havard.tveite@nmbu.no>
2020-04-02 01:03:37 +0200

    rebase fix

--- resource_sharing/gui/resource_sharing_dialog.py
+++ resource_sharing/gui/resource_sharing_dialog.py
@@ -197,17 +197,10 @@ class ResourceSharingDialog(QDialog, FORM_CLASS):
         # Clear message bar first
         self.message_bar.clearWidgets()
         if index == (self.menu_list_widget.count() - 1):
-<<<<<<< HEAD
             # Last menu entry - Settings
             self.stacked_menu_widget.setCurrentIndex(1)
         else:
             # Not settings, must be Collections
-=======
-            # Switch to the settings page
-            self.stacked_menu_widget.setCurrentIndex(1)
-        else:
-            # Switch to the collections page
->>>>>>> testing
             if index == 1:
                 # Installed collections
                 self.collection_proxy.accepted_status = \
@@ -528,11 +521,15 @@ class ResourceSharingDialog(QDialog, FORM_CLASS):
             LOGGER.error('Could not uninstall collection ' +
                          config.COLLECTIONS[coll_id]['name'])
         else:
-            #self._selected_collection_id = None
             self.reload_collections_model()
+<<<<<<< HEAD
             currentRow = self.menu_list_widget.currentRow()
             self.set_current_tab(currentRow)
             self.set_current_tab(0)
+=======
+            currentPage = self.stacked_menu_widget.getCurrentIndex(1)
+            self.set_current_tab(currentPage)
+>>>>>>> testing
             QMessageBox.information(
                 self,
                 'Resource Sharing',

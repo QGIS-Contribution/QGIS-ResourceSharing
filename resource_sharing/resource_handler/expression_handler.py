b03f3068f054349ab9e609d13ca17ba48fd06698
Håvard Tveite <havard.tveite@nmbu.no>
2020-05-13 14:12:04 +0200

    Fixing

--- resource_sharing/resource_handler/expression_handler.py
+++ resource_sharing/resource_handler/expression_handler.py
@@ -1,6 +1,15 @@
 # coding=utf-8
 from pathlib import Path
+import json
 import logging
+
+from qgis.core import QgsSettings
+try:
+    from qgis.core import Qgis
+except ImportError:
+    from qgis.core import QGis as Qgis
+#try:
+#    from qgis.core import QgsExpressions
 
 from resource_sharing.resource_handler.base import BaseResourceHandler
 from resource_sharing.utilities import qgis_version
@@ -31,22 +40,35 @@ class ExpressionHandler(BaseResourceHandler):
         # Uninstall first (in case it is a reinstall)
         self.uninstall()
 
+        settings = QgsSettings()
+        settings.beginGroup(user_expressions_group())
+
         # Get all the expressions in the collection
         json_files = []
         valid = 0
-        for item in Path(self.resource_dir).glob('*.json'):
-            file_path = Path(self.resource_dir, item)
-            json_files.append(file_path)
-            valid += 1
+        for item in Path(self.resource_dir).glob('*'):
+            if item.suffix.lower().endswith("json"):
+                file_path = Path(self.resource_dir, item)
+                json_files.append(file_path)
+                valid += 1
 
         # If there are no json files, there is nothing to do
         if len(json_files) == 0:
             return
 
         for json_file in json_files:
-            file_name = json_file.stem
-            LOGGER.info("Installing expressions from " + file_name)
+            #file_name = json_file.stem
+            LOGGER.info("Installing expressions from " + json_file)
+
+            with open(json_file, 'rb') as expr_file:
+                expr_json = expr_file.read()
+            jsontext = json.loads(expr_json)
+            expressions = jsontext['expressions']
+            for expr in expressions:
+                settings.setValue(expr['name'] + '\expression', expr['expression'])
+                settings.setValue(expr['name'] + '\helpText', expr['description'])
             valid += 1
+        settings.endGroup()
         if valid >= 0:
             self.collection[EXPRESSIONS] = valid
 
@@ -56,5 +78,8 @@ class ExpressionHandler(BaseResourceHandler):
 
         # Remove from settings
         LOGGER.info("Removing expressions")
+        settings = QgsSettings()
+        settings.beginGroup(user_expressions_group())
+        settings.endGroup()
         # Remove files - nothing to remove
 
